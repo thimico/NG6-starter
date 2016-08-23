06bee21e459b4bbeaa1bc93cfa608f84ae0839b2
Taranto <rafael.taranto5@gmail.com>
2016-08-23 17:03:57 -0300

    modal

--- client/app/components/banco/banco.edit.modal/banco.edit.modal.controller.js
+++ client/app/components/banco/banco.edit.modal/banco.edit.modal.controller.js
@@ -1,6 +1,6 @@
-import BaseModalEditController from '../../base/base.modal.edit.controller';
+import BaseEditController from '../../../base/base.edit.controller';
 
-class BancoEditController extends BaseModalEditController{
+class BancoEditController extends BaseEditController{
   /*@ngInject*/
   constructor(bancoService, ModalParams, $scope) {
     super(bancoService, ModalParams, $scope);
